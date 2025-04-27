729665108773a7c20d46306f5b0879e1cbb74a4d
Fatima Sharifi <ftshn84@gmail.com>
2025-04-27 14:31:48 +0200

    Creating responsive layout using Chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { ThemeProvider } from 'next-themes'
+
 import App from './App'
 import './index.css'
+import { Provider } from './components/ui/provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+
+      <App />
+
+    </Provider>
   </React.StrictMode>,
 )
