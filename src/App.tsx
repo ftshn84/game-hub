1d9ed497fffebecacf5ed522531ee805c8869312
Fatima Sharifi <ftshn84@gmail.com>
2025-05-01 17:34:35 +0200

    setup priteer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react"
+import { Button, ButtonGroup } from "@chakra-ui/react"
 
 function App() {
 
 
 
 
-  return <Grid templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "main aside"` }}
-  >
+  
 
-    <GridItem area="nav" bg="blue" display={{ base: "block", lg: "none" }}>
-      Nav
-    </GridItem>
-    <GridItem area="aside" bg="red" display={{ base: "block", lg: "none" }}>
-      Aside
-    </GridItem>
-    <GridItem area="main" bg="blue">
-      Main
-    </GridItem>
-  </Grid>
+  return <Button colorScheme='blue'>Click me</Button>
 
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from "@chakra-ui/react";
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ThemeProvider } from "next-themes";
 
-import { ThemeProvider } from 'next-themes'
+import App from "./App";
+import "./index.css";
+import { Provider } from "./components/ui/provider";
 
-import App from './App'
-import './index.css'
-import { Provider } from './components/ui/provider'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider>
-
-      <App />
-
-    </Provider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode> </React.StrictMode>
+);
