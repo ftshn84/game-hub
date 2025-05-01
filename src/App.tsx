7c9957c20d47d631892700b8a1f036c9543e2869
Fatima Sharifi <ftshn84@gmail.com>
2025-05-01 09:30:50 +0200

    install Chakra.ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { Grid, GridItem } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
 
-  return <Grid templateAreas={`"nav   nav" "aside main"`}>
-    <GridItem area='nav' bg='coral'>Nav  </GridItem>
-    <GridItem area='aside' bg='gold'>Aside  </GridItem>
-    <GridItem area='main' bg='dadgerblue'>Main  </GridItem>
+
+
+
+  return <Grid templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "main aside"` }}
+  >
+
+    <GridItem area="nav" bg="blue" display={{ base: "block", lg: "none" }}>
+      Nav
+    </GridItem>
+    <GridItem area="aside" bg="red" display={{ base: "block", lg: "none" }}>
+      Aside
+    </GridItem>
+    <GridItem area="main" bg="blue">
+      Main
+    </GridItem>
   </Grid>
+
 }
 
 export default App;
