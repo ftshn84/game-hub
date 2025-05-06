ef250827c4355c1b8698bb82ba077600bb33ecd3
Fatima Sharifi <ftshn84@gmail.com>
2025-05-06 12:53:45 +0200

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,28 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../assets/services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres(); // Assuming useGenres is imported correctly
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={2} paddingX={4} cursor="pointer">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+              objectFit="cover"
+              alt={genre.name}
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              {genre.name}
+            </Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
