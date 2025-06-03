2800fc3952785a9d4bf3bffe3dcf6eefba21c8eb
Fatima Sharifi <ftshn84@gmail.com>
2025-06-03 21:06:28 +0200

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -39,23 +40,26 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Flex paddingLeft={2} marginBottom={5}>
-          <Box marginRight={5}>
-            <PlatformSelector
-              selectedPlatform={gameQuery.platform}
-              onSelectPlatform={(platform) =>
-                seGameQuery({ ...gameQuery, platform })
-              } // Update the state with the selected platform
-            />
-          </Box>
-          <SortSelector
-            onSelectSortOrder={(sortOrder) =>
-              seGameQuery({ ...gameQuery, sortOrder })
-            } // Update the state with the selected sort order
-            sortOrder={gameQuery.sortOrder} // Pass the current sort order to SortSelector
-          />
-        </Flex>
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
 
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                selectedPlatform={gameQuery.platform}
+                onSelectPlatform={(platform) =>
+                  seGameQuery({ ...gameQuery, platform })
+                } // Update the state with the selected platform
+              />
+            </Box>
+            <SortSelector
+              onSelectSortOrder={(sortOrder) =>
+                seGameQuery({ ...gameQuery, sortOrder })
+              } // Update the state with the selected sort order
+              sortOrder={gameQuery.sortOrder} // Pass the current sort order to SortSelector
+            />
+          </Flex>
+        </Box>
         <GameGrid
           gameQuery={gameQuery} // Pass the GameQuery state to GameGrid
         />
