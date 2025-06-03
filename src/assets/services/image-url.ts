b767d890de24ddc458f76c63503fafca32708c8e
Fatima Sharifi <ftshn84@gmail.com>
2025-06-03 16:26:12 +0200

    fixed show the game cards without pictures

--- src/assets/services/image-url.ts
+++ src/assets/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "@/assets/no-image-placeholder.webp"; // Import a placeholder image for cases where the URL is empty or undefined
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage; // Return a placeholder image if the URL is empty or undefined
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
