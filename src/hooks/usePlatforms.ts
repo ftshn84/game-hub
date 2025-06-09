165a37cd54efe6fd3d14e83b018be01775c9be22
Fatima Sharifi <ftshn84@gmail.com>
2025-06-09 23:50:12 +0200

    Error of platfrom (it was used two times)

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,5 @@
 import useData from "./useData";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 const usePlatforms = () => {
   return useData<Platform>("/platforms/lists/parents");
 };
